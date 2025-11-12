555fef8a373980c0eb934c0947608e90b5aa9222
yyueda <yyuedaa@gmail.com>
2025-11-12 20:31:58 +0800

    Set up base skeleton code

---
+++ src/centpai/config.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", case_sensitive=True)
+
+    BOT_TOKEN: str
+
+settings = Settings()
