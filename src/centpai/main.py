fdddef8130b61c0afd09566fde4f614db79d5d4d
CalvinChuaYH <calvinchuayh@gmail.com>
2026-01-02 18:21:18 +0800

    feat(webhook): initialise webhook and welcome message

--- src/centpai/main.py
+++ src/centpai/main.py
@@ -1,13 +1,41 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from logging_config import setup_logging
+from telegram import client
+from config import settings
 
 setup_logging()
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup():
+    tg = client.TelegramAPI(settings.BOT_TOKEN)
+    await tg.set_webhook(
+        url="https://dc0e891a6de9.ngrok-free.app/webhook", 
+        secret_token="test_secret")
+    app.state.telegram = tg
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/webhook")
+async def read_webhook(update: dict, request: Request):
+    if "my_chat_member" in update:
+        event = update["my_chat_member"]
+
+        old_status = event["old_chat_member"]["status"]
+        new_status = event["new_chat_member"]["status"]
+
+        if old_status in ("kicked", "left") and new_status in ("member", "administrator"):
+            #bot just added to the group, send welcome message
+            chat_id = event["chat"]["id"]
+            tg = request.app.state.telegram
+            await tg.send_welcome_message(chat_id=chat_id)
+            
+    return {"ok": True}
 
 
 @app.get("/items/{item_id}")
--- src/centpai/telegram/client.py
+++ src/centpai/telegram/client.py
@@ -1,6 +1,6 @@
 import httpx
 import logging
-from ..config import settings
+from centpai.config import settings
 from typing import Any, Dict
 
 logger = logging.getLogger("telegram")
@@ -20,13 +20,18 @@ class TelegramAPI:
         self, 
         chat_id: int, 
         text: str, 
-        reply_to_message_id: int | None = None, 
+        reply_to_message_id: int | None = None,
+        reply_markup: dict | None=None,
         parse_mode: str | None = "HTML"
     ) -> Dict[str, Any]:
+        
         payload: Dict[str, Any] = {
             "chat_id": chat_id,
             "text": text,
         }
+
+        if reply_markup:
+            payload["reply_markup"] = reply_markup
         if parse_mode: payload["parse_mode"] = parse_mode
         if reply_to_message_id: 
             payload["reply_parameters"] = {"message_id": reply_to_message_id}
@@ -69,4 +74,21 @@ class TelegramAPI:
         r = await self._client.get(f"/getUpdates", params=params)
         return r.json()
 
-tg = TelegramAPI(settings.BOT_TOKEN)
+    async def send_welcome_message(self, chat_id:str):
+        text = (
+            "Welcome to Centpai!\n\n"
+            "Tap a button below to get started:"
+        )
+
+        keyboard = {
+            "inline_keyboard": [
+                [
+                    {"text": "Join Group", "callback_data": "join_group"}
+                ],
+                [
+                    {"text": "How it works", "callback_data": "how_it_works"}
+                ]
+            ]
+        }
+
+        await self.send_message(chat_id=chat_id, text=text, reply_markup=keyboard)
