98c1ee512f19e8e5bbc07e5ebbf0f8fcfa22aee3
CalvinChuaYH <calvinchuayh@gmail.com>
2026-01-06 16:12:34 +0800

    feat(group): update group functionality, add expense interface

--- app/features/telegram/client.py
+++ app/features/telegram/client.py
@@ -1,6 +1,7 @@
 import httpx
 import logging
 from typing import Any, Dict
+import collections
 
 logger = logging.getLogger("telegram")
 
@@ -10,6 +11,8 @@ class TelegramAPI:
     def __init__(self, token: str, timeout: float = 10.0):
         self.base = f"{BASE}/bot{token}"
         self._client = httpx.AsyncClient(base_url=self.base, timeout=timeout)
+        self.group = collections.defaultdict(set)
+        self.expenses = collections.defaultdict(int)
     
     async def aclose(self) -> None:
         """Close the underlying HTTP client (e.g. on shutdown)."""
@@ -92,3 +95,95 @@ class TelegramAPI:
         }
 
         await self.send_message(chat_id=chat_id, text=text, reply_markup=keyboard)
+    
+    async def send_group_message(self, chat_id: int):
+        current_members = self.group[chat_id]
+        text = (
+            "Welcome to Centpai!\n\n"
+            "Current members:\n"
+            + "\n".join(f"• {member}" for member in current_members) +
+            "\n\nExpenses:\n"
+        )
+
+        keyboard = {
+            "inline_keyboard": [
+                [
+                    {"text": "Join Group", "callback_data": "join_group"}
+                ],
+                [
+                    {"text": "Leave Group", "callback_data": "leave_group"}
+                ],
+                [
+                    {"text": "Add Expense", "callback_data": "add_expense"}
+                ],
+                [
+                    {"text": "Remove Expense", "callback_data": "remove_expense"}
+                ],
+                [
+                    {"text": "Settle Up", "callback_data": "settle_up"}
+                ],
+                [
+                    {"text": "How it works", "callback_data": "how_it_works"}
+                ]
+            ]
+        }
+
+        await self.send_message(chat_id=chat_id, text=text, reply_markup=keyboard)
+    
+
+    async def send_expense_message(self, chat_id: int):
+        current_members = self.group[chat_id]
+        text = (
+            "Welcome to Centpai!\n\n"
+            "Current members:\n"
+            + "\n".join(f"• {member}" for member in current_members) +
+            "\n\nExpenses:\n"
+        )
+
+        keyboard = {
+            "inline_keyboard": [
+                [
+                    {"text": "Food & Drink", "callback_data": "add_fooddrink_expense"}
+                ],
+                [
+                    {"text": "Transport", "callback_data": "add_transport_expense"}
+                ],
+                [
+                    {"text": "Accomodation", "callback_data": "add_accomodation_expense"}
+                ],
+                [
+                    {"text": "Entertainment", "callback_data": "add_entertainment_expense"}
+                ],
+                [
+                    {"text": "Shopping", "callback_data": "add_shopping_expense"}
+                ],
+                [
+                    {"text": "Others", "callback_data": "add_others_expense"}
+                ]
+            ]
+        }
+
+        await self.send_message(chat_id=chat_id, text=text, reply_markup=keyboard)
+    
+    async def answer_callback_query(self, callback_query_id: str, text: str | None = None, show_alert: bool = False, url: str | None = None, cache_time: int = 0):
+        payload = {
+            "callback_query_id": callback_query_id
+        }
+
+        if text:
+            payload["text"] = text
+        if show_alert:
+            payload["show_alert"] = show_alert
+        if url:
+            payload["url"] = url
+        if cache_time:
+            payload["cache_time"] = cache_time
+        
+        await self._client.post(f"/answerCallbackQuery", json=payload)
+    
+    def add_user_to_group(self, username: str, chat_id: int):
+        self.group[chat_id].add(username)
+    
+
+    def remove_user_from_group(self, username: str, chat_id: int):
+        self.group[chat_id].discard(username)
--- app/main.py
+++ app/main.py
@@ -5,15 +5,16 @@ from features.telegram import client
 from core.config import settings
 
 setup_logging()
-app = FastAPI()
 
-@app.on_event("startup")
-async def startup():
+async def lifespan(app: FastAPI):
     tg = client.TelegramAPI(settings.BOT_TOKEN)
     await tg.set_webhook(
-        url="https://dc0e891a6de9.ngrok-free.app/webhook", 
+        url="https://0e5d760fddfa.ngrok-free.app/webhook", 
         secret_token="test_secret")
     app.state.telegram = tg
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
@@ -23,6 +24,8 @@ def read_root():
 
 @app.post("/webhook")
 async def read_webhook(update: dict, request: Request):
+    tg = request.app.state.telegram
+
     if "my_chat_member" in update:
         event = update["my_chat_member"]
 
@@ -32,8 +35,33 @@ async def read_webhook(update: dict, request: Request):
         if old_status in ("kicked", "left") and new_status in ("member", "administrator"):
             #bot just added to the group, send welcome message
             chat_id = event["chat"]["id"]
-            tg = request.app.state.telegram
             await tg.send_welcome_message(chat_id=chat_id)
+
+    if "callback_query" in update:
+        cq = update["callback_query"]
+
+        callback_id = cq["id"]
+        data = cq.get("data")          
+        chat_id = cq["message"]["chat"]["id"]
+        username = cq["from"]["username"]
+
+        if data == "join_group":
+            tg.add_user_to_group(username=username, chat_id=chat_id)
+            await tg.send_message(chat_id=chat_id, text=f"{username} joined the group.")
+            await tg.send_group_message(chat_id=chat_id)
+        elif data == "leave_group":
+            tg.remove_user_from_group(username=username, chat_id=chat_id)
+            await tg.send_message(chat_id=chat_id, text=f"{username} left the group.")
+            await tg.send_group_message(chat_id=chat_id)
+        elif data == "add_expense":
+            await tg.send_expense_message(chat_id=chat_id)
+        elif data == "how_it_works":
+            await tg.send_message(chat_id=chat_id, text="Centpai works like this: ...")
+        
+        
+        await tg.answer_callback_query(callback_query_id=callback_id)
+
+        return {"ok": True}
             
     return {"ok": True}
 
