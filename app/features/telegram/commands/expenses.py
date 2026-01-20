0997dea35a49abcfb7fb47fa35b90a2c4dbdf6c3
CalvinChuaYH <calvinchuayh@gmail.com>
2026-01-20 17:44:04 +0800

    feat(expenses): added handle list expenses

--- app/features/telegram/commands/expenses.py
+++ app/features/telegram/commands/expenses.py
@@ -1,6 +1,6 @@
 from decimal import ROUND_HALF_UP, Decimal, InvalidOperation
 from app.core.errors import DomainError
-from app.features.expenses.errors import ServerError
+from app.features.expenses.errors import ServerError, ChatNotFound
 from app.features.expenses.service import ExpensesService
 from app.features.telegram.client import Messenger
 from app.features.telegram.context import TgContext
@@ -48,17 +48,49 @@ def parse_amount(amount: str) -> Decimal:
     except InvalidOperation:
         raise ValueError("Invalid amount format")
 
-async def handleListExpenses(
-    ctx: TgContext, 
-    messenger: Messenger, 
-    svc: ExpensesService
-) -> None:
+# async def handleListExpenses(
+#     ctx: TgContext, 
+#     messenger: Messenger, 
+#     svc: ExpensesService
+# ) -> None:
+#     try:
+#         expenses = await svc.get_expenses(ctx.tg_chat_id)
+
+#     except DomainError as e:
+#         await messenger.send_message(
+#             ctx.tg_chat_id,
+#             e.message,
+#             ctx.message_id
+#         )
+async def handleListExpenses(ctx: TgContext, messenger: Messenger, svc: ExpensesService) -> None:
     try:
         expenses = await svc.get_expenses(ctx.tg_chat_id)
-        
+        if expenses:
+            message_lines = ["Recent expenses:"]
+            for exp in expenses:
+                #TODO: add participants info
+                message_lines.append(f"• {exp.paid_by} paid {exp.amount} for {exp.desc} on {exp.created_at.strftime('%Y-%m-%d')}")
+
+            await messenger.send_message(
+                chat_id=ctx.tg_chat_id,
+                text="\n".join(message_lines),
+                reply_to_message_id=ctx.message_id
+            )
+        else:
+            await messenger.send_message(
+                chat_id=ctx.tg_chat_id,
+                text="No expenses found.",
+                reply_to_message_id=ctx.message_id
+            )
+    except ChatNotFound as e:
+        await messenger.send_message(
+            chat_id=ctx.tg_chat_id,
+            text=f"{e.message}",
+            reply_to_message_id=ctx.message_id
+        )
     except DomainError as e:
         await messenger.send_message(
-            ctx.tg_chat_id,
-            e.message,
-            ctx.message_id
+            chat_id=ctx.tg_chat_id,
+            text=f"{e.message}",
+            reply_to_message_id=ctx.message_id
         )
