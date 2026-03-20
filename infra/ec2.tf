d728add4a69328eea37e4a22065dcee49847c346
yyueda <yyuedaa@gmail.com>
2026-03-20 18:41:17 +0800

    feat(infra): add Terraform files for EC2 instance

---
+++ infra/ec2.tf
@@ -0,0 +1,53 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+resource "aws_security_group" "centpai" {
+  name        = "centpai-sg"
+  description = "Allow SSH and HTTPS for centpai"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "centpai" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t3.micro"
+  key_name               = "centpai"
+  vpc_security_group_ids = [aws_security_group.centpai.id]
+
+  tags = {
+    Name = "centpai"
+  }
+}
