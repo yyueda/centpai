bf901c17a198a9e92c255b58ac2bd3d475e6029d
yyueda <yyuedaa@gmail.com>
2026-03-21 19:55:19 +0800

    fix: edit terraform confif for aws security group

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -20,15 +20,15 @@ resource "aws_security_group" "centpai" {
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
