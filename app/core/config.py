ee84b13f15910d277f10e8b71c363b67fabd6a33
CalvinChuaYH <calvinchuayh@gmail.com>
2026-03-26 15:20:26 +0800

    refactor: clean up

--- app/core/config.py
+++ app/core/config.py
@@ -8,11 +8,7 @@ class Settings(BaseSettings):
 
     BOT_TOKEN: str
     DATABASE_URL: str
-<<<<<<< HEAD
     WEBHOOK_URL: str
-=======
-    NGROK_URL: str
->>>>>>> cc15d5b (chore: clean up and store all variables in env)
     WEBHOOK_SECRET: str
 
 
